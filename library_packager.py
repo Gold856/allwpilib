c446539ec564b66b91c463531d8b940cf0c24c97
Gold856 <117957790+Gold856@users.noreply.github.com>
2024-07-01 02:29:54 -0400

    Add Python script to package libraries

---
+++ library_packager.py
@@ -0,0 +1,56 @@
+import argparse
+import glob
+import os
+import subprocess
+import sys
+import zipfile
+from pathlib import Path
+
+
+def main(argv):
+    dirname = Path(__file__).resolve().parent
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--install_directory",
+        help="Required.",
+        type=Path,
+    )
+
+    args = parser.parse_args(argv)
+    out = subprocess.run(["git", "describe"], capture_output=True, text=True)
+    version = out.stdout[1:].strip()
+    directories = glob.glob("*", root_dir=args.install_directory)
+    for dir in directories:
+        # TODO: Remove
+        if dir == "include" or dir == "java" or dir == "jni" or dir == "share":
+            continue
+        os.chdir(args.install_directory / dir)
+        files = glob.glob("**", recursive=True)
+        debug_or_not = ""
+        static_or_not = ""
+        platform = ""
+        arch = ""
+        for file in files:
+            if "debug" in file:
+                debug_or_not = "debug"
+            if "static" in file:
+                static_or_not = "static"
+            # We need a complete platform path to calculate OS and arch
+            if len(platform_parts := file.split("\\")) >= 3:
+                platform = platform_parts[0]
+                arch = platform_parts[1]
+        artifact_name = (
+            f"{dir}-cpp-{version}-{platform}{arch}{static_or_not}{debug_or_not}"
+        )
+        with zipfile.ZipFile(
+            args.install_directory / f"{artifact_name}.zip",
+            "w",
+        ) as archive:
+            for file in files:
+                archive.write(file)
+            archive.write(dirname / "ThirdPartyNotices.txt", "ThirdPartyNotices.txt")
+            archive.write(dirname / "LICENSE.md", "LICENSE.md")
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
