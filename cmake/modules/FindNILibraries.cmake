0c0f6c0ebe06d0aa49a5601bcb1edf8a3aeebaf0
Gold856 <117957790+Gold856@users.noreply.github.com>
2024-07-10 16:10:59 -0400

    Fix CI

--- cmake/modules/FindNILibraries.cmake
+++ cmake/modules/FindNILibraries.cmake
@@ -1,15 +1,15 @@
 include(PlatformVars)
-if(NILibraries_FOUND)
-elseif(BUILD_MAVEN)
-    include(DownloadAndCheck)
-    set(base_url https://frcmaven.wpi.edu/artifactory/release/edu/wpi/first/ni-libraries)
-    set(version 2024.2.1)
-    set(libs chipobject netcomm visa)
-    set(dest ${WPILIB_BINARY_DIR}/ni-libraries)
-    foreach(lib ${libs})
-        set(base_artifact_name ${lib}-${version}-linuxathena)
-        set(library_artifact ${base_artifact_name}-linuxathena)
-        set(headers_artifact ${base_artifact_name}-headers)
+include(DownloadAndCheck)
+set(base_url https://frcmaven.wpi.edu/artifactory/release/edu/wpi/first/ni-libraries)
+set(version 2024.2.1)
+set(libs chipobject netcomm visa)
+set(dest ${WPILIB_BINARY_DIR}/ni-libraries)
+message(STATUS "Downloading and unpacking NI libraries.")
+foreach(lib ${libs})
+    set(base_artifact_name ${lib}-${version})
+    set(library_artifact ${base_artifact_name}-linuxathena)
+    set(headers_artifact ${base_artifact_name}-headers)
+    if(NOT NILibraries_FOUND)
         if(NOT EXISTS ${dest}/${library_artifact}.zip)
             download_and_check(
                 ${base_url}/${lib}/${version}/${library_artifact}.zip
@@ -28,14 +28,15 @@ elseif(BUILD_MAVEN)
             INPUT ${dest}/${headers_artifact}.zip
             DESTINATION ${dest}/${lib}/include
         )
-        file(GLOB ni_lib ${dest}/${lib}/linux/athena/*)
-        add_library(${lib} SHARED IMPORTED GLOBAL)
-        target_include_directories(${lib} INTERFACE ${dest}/${lib}/include)
-        set_target_properties(
-            ${lib}
-            PROPERTIES IMPORTED_LOCATION_DEBUG ${ni_lib} IMPORTED_LOCATION_RELWITHDEBINFO ${ni_lib}
-        )
-        list(APPEND NI_LIBRARIES ${lib})
-    endforeach()
-    set(NILibraries_FOUND true)
-endif()
+    endif()
+    file(GLOB ni_lib ${dest}/${lib}/linux/athena/shared/*)
+    add_library(${lib} SHARED IMPORTED)
+    target_include_directories(${lib} INTERFACE ${dest}/${lib}/include)
+    set_target_properties(
+        ${lib}
+        PROPERTIES IMPORTED_LOCATION_DEBUG ${ni_lib} IMPORTED_LOCATION_RELWITHDEBINFO ${ni_lib}
+    )
+    list(APPEND NI_LIBRARIES ${lib})
+endforeach()
+message(STATUS "Done.")
+set(NILibraries_FOUND true PARENT_SCOPE)
