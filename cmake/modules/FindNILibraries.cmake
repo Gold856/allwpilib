45affc71bebc3bef4229b91a66fd2517fc000f10
Gold856 <117957790+Gold856@users.noreply.github.com>
2024-07-20 04:31:41 -0400

    Refactor find modules to use FetchContent

--- cmake/modules/FindNILibraries.cmake
+++ cmake/modules/FindNILibraries.cmake
@@ -1,5 +1,5 @@
+include(FetchContent)
 include(PlatformVars)
-include(DownloadAndCheck)
 set(base_url https://frcmaven.wpi.edu/artifactory/release/edu/wpi/first/ni-libraries)
 set(version 2024.2.1)
 set(libs chipobject netcomm runtime visa)
@@ -7,36 +7,38 @@ set(dest ${WPILIB_BINARY_DIR}/ni-libraries)
 message(STATUS "Downloading and unpacking NI libraries.")
 foreach(lib ${libs})
     set(base_artifact_name ${lib}-${version})
-    set(library_artifact ${base_artifact_name}-linuxathena)
-    set(headers_artifact ${base_artifact_name}-headers)
-    if(NOT NILibraries_FOUND)
-        if(NOT EXISTS ${dest}/${library_artifact}.zip)
-            download_and_check(
-                ${base_url}/${lib}/${version}/${library_artifact}.zip
-                ${dest}/${library_artifact}.zip
-            )
-        endif()
-        if(NOT EXISTS ${dest}/${headers_artifact}.zip AND (NOT ${lib} STREQUAL runtime))
-            download_and_check(
-                ${base_url}/${lib}/${version}/${headers_artifact}.zip
-                ${dest}/${headers_artifact}.zip
-            )
-        endif()
-        file(ARCHIVE_EXTRACT INPUT ${dest}/${library_artifact}.zip DESTINATION ${dest}/${lib})
-        if(NOT ${lib} STREQUAL runtime)
-            file(
-                ARCHIVE_EXTRACT
-                INPUT ${dest}/${headers_artifact}.zip
-                DESTINATION ${dest}/${lib}/include
-            )
-        endif()
+    set(dest ${WPILIB_BINARY_DIR}/ni-libraries/${lib})
+    fetchcontent_declare(
+        ${lib}
+        URL ${base_url}/${lib}/${version}/${base_artifact_name}-linuxathena.zip
+        DOWNLOAD_NO_EXTRACT true
+    )
+    if(NOT ${lib} STREQUAL runtime)
+        fetchcontent_declare(
+            ${lib}_headers
+            URL ${base_url}/${lib}/${version}/${base_artifact_name}-headers.zip
+            DOWNLOAD_NO_EXTRACT true
+        )
+        fetchcontent_makeavailable(${lib}_headers)
+        file(
+            ARCHIVE_EXTRACT
+            INPUT ${${lib}_headers_SOURCE_DIR}/${base_artifact_name}-headers.zip
+            DESTINATION ${dest}/include
+        )
     endif()
-    file(GLOB ni_libs ${dest}/${lib}/linux/athena/shared/*)
+    fetchcontent_makeavailable(${lib})
+    file(
+        ARCHIVE_EXTRACT
+        INPUT ${${lib}_SOURCE_DIR}/${base_artifact_name}-linuxathena.zip
+        DESTINATION ${dest}
+    )
+
+    file(GLOB ni_libs ${dest}/linux/athena/shared/*)
     foreach(ni_lib ${ni_libs})
         cmake_path(GET ni_lib STEM lib_name)
         add_library(${lib_name} SHARED IMPORTED)
         if(NOT ${lib} STREQUAL runtime)
-            target_include_directories(${lib_name} INTERFACE ${dest}/${lib}/include)
+            target_include_directories(${lib_name} INTERFACE ${dest}/include)
         endif()
         set_target_properties(
             ${lib_name}
