66ace30c1f81f3b1c10d4b342b12e1439f1a885b
Gold856 <117957790+Gold856@users.noreply.github.com>
2024-07-20 04:31:41 -0400

    Refactor find modules to use FetchContent

--- cmake/modules/FindOpenCV.cmake
+++ cmake/modules/FindOpenCV.cmake
@@ -1,33 +1,22 @@
-include(PlatformVars)
-if(OpenCV_FOUND)
-elseif(BUILD_MAVEN)
-    include(DownloadAndCheck)
+if(BUILD_MAVEN)
+    include(FetchContent)
+    include(PlatformVars)
     set(base_url
-        https://frcmaven.wpi.edu/artifactory/release/edu/wpi/first/thirdparty/frc2024/opencv/opencv-cpp/4.8.0-4
+        https://frcmaven.wpi.edu/artifactory/release/edu/wpi/first/thirdparty/frc2024/opencv
     )
+    set(base_native_url ${base_url}/opencv-cpp/4.8.0-4)
     set(base_artifact_name opencv-cpp-4.8.0-4)
     set(library_artifact ${base_artifact_name}-${platform}${arch})
-    set(headers_artifact ${base_artifact_name}-headers)
-    set(dest ${WPILIB_BINARY_DIR}/opencv)
     message(STATUS "Downloading and unpacking OpenCV artifacts.")
-    if(NOT EXISTS ${dest}/${library_artifact}.zip)
-        download_and_check(${base_url}/${library_artifact}.zip ${dest}/${library_artifact}.zip)
-    endif()
-    if(NOT EXISTS ${dest}/${headers_artifact}.zip)
-        download_and_check(${base_url}/${headers_artifact}.zip ${dest}/${headers_artifact}.zip)
-    endif()
-    if(NOT EXISTS ${dest}/${library_artifact}debug.zip)
-        download_and_check(
-            ${base_url}/${library_artifact}debug.zip
-            ${dest}/${library_artifact}debug.zip
-        )
-    endif()
 
-    file(ARCHIVE_EXTRACT INPUT ${dest}/${library_artifact}.zip DESTINATION ${dest}/release)
-    file(ARCHIVE_EXTRACT INPUT ${dest}/${library_artifact}debug.zip DESTINATION ${dest}/debug)
+    fetchcontent_declare(opencv_debug URL ${base_native_url}/${library_artifact}debug.zip)
+    fetchcontent_declare(opencv_release URL ${base_native_url}/${library_artifact}.zip)
+    fetchcontent_declare(opencv_headers URL ${base_native_url}/${base_artifact_name}-headers.zip)
+    fetchcontent_makeavailable(opencv_debug opencv_release opencv_headers)
+
     if(MSVC)
-        file(GLOB_RECURSE opencv_release_libs ${dest}/release/*.dll)
-        file(GLOB_RECURSE opencv_debug_libs ${dest}/debug/*.dll)
+        file(GLOB_RECURSE opencv_release_libs ${opencv_release_SOURCE_DIR}/*.dll)
+        file(GLOB_RECURSE opencv_debug_libs ${opencv_debug_SOURCE_DIR}/*.dll)
         get_property(isMultiConfig GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
         if(isMultiConfig)
             file(
@@ -40,7 +29,6 @@ elseif(BUILD_MAVEN)
             file(COPY ${opencv_debug_libs} DESTINATION ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
         endif()
     endif()
-    file(ARCHIVE_EXTRACT INPUT ${dest}/${headers_artifact}.zip DESTINATION ${dest}/include)
     message(STATUS "Done.")
     set(libs
         aruco
@@ -62,9 +50,9 @@ elseif(BUILD_MAVEN)
     )
     foreach(lib ${libs})
         add_library(opencv_${lib} SHARED IMPORTED GLOBAL)
-        set(debug_lib_location ${dest}/debug/${platform}/${arch}/shared)
-        set(release_lib_location ${dest}/release/${platform}/${arch}/shared)
-        target_include_directories(opencv_${lib} INTERFACE ${dest}/include)
+        set(debug_lib_location ${opencv_debug_SOURCE_DIR}/${platform}/${arch}/shared)
+        set(release_lib_location ${opencv_release_SOURCE_DIR}/${platform}/${arch}/shared)
+        target_include_directories(opencv_${lib} INTERFACE ${opencv_headers_SOURCE_DIR})
         if(MSVC)
             set_target_properties(
                 opencv_${lib}
@@ -93,11 +81,13 @@ elseif(BUILD_MAVEN)
         list(APPEND OpenCV_LIBS opencv_${lib})
     endforeach()
     if(WITH_JAVA)
-        download_and_check(
-            "https://frcmaven.wpi.edu/artifactory/release/edu/wpi/first/thirdparty/frc2024/opencv/opencv-java/4.8.0-4/opencv-java-4.8.0-4.jar"
-            ${dest}/opencv-java-4.8.0-4.jar
+        fetchcontent_declare(
+            opencv_jar
+            URL ${base_url}/opencv-java/4.8.0-4/opencv-java-4.8.0-4.jar
+            DOWNLOAD_NO_EXTRACT ON
         )
-        set(OPENCV_JAR_FILE ${dest}/opencv-java-4.8.0-4.jar)
+        fetchcontent_makeavailable(opencv_jar)
+        set(OPENCV_JAR_FILE ${opencv_jar_SOURCE_DIR}/opencv-java-4.8.0-4.jar)
     endif()
     set(OpenCV_FOUND true)
 else()
