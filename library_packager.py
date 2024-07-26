d560fff2f1bfb8faa0926ea5c5cb67a413ff01cb
Gold856 <117957790+Gold856@users.noreply.github.com>
2024-07-26 03:05:14 -0400

    Install everything to artifact dirs, make less assumptions in Python script Also add more info to CMake build

--- library_packager.py
+++ library_packager.py
@@ -64,9 +64,11 @@ def package_artifacts(install_dir: Path, repo_root: Path):
 
     directories = glob.glob("*", root_dir=install_dir)
     for dir in directories:
-        if dir in ["include", "java", "share", "sources"]:
+        if dir in ["java", "bin", "lib", "share"] or "zip" in dir:
             continue
         os.chdir(install_dir / dir)
+        with open("group_info.txt") as f:
+            group = f.read()
         files = glob.glob("**", recursive=True)
 
         # Identify how these libraries were built
@@ -75,44 +77,50 @@ def package_artifacts(install_dir: Path, repo_root: Path):
         platform = ""
         arch = ""
         for file in files:
+            if "include" in file or "sources" in file:
+                continue
             if "debug" in file:
                 debug_or_not = "debug"
             if "static" in file:
                 static_or_not = "static"
-            # We need a complete platform path to calculate OS and arch
+                # We need a complete platform path to calculate OS and arch
             if len(platform_parts := file.replace("\\", "/").split("/")) >= 3:
                 platform = platform_parts[0]
                 arch = platform_parts[1]
         artifact_name = (
-            f"{dir}-cpp-{version}-{platform}{arch}{static_or_not}{debug_or_not}"
+            f"_GROUP_{group}_ID_{dir}_CLS_{platform}{arch}{static_or_not}{debug_or_not}"
         )
 
         with zipfile.ZipFile(install_dir / f"{artifact_name}.zip", "w") as archive:
             for file in files:
+                if file == "group_info.txt" or "include" in file or "sources" in file:
+                    continue
                 archive.write(file)
             archive.write(repo_root / "ThirdPartyNotices.txt", "ThirdPartyNotices.txt")
             archive.write(repo_root / "LICENSE.md", "LICENSE.md")
 
-    create_archive(repo_root, install_dir, version, "include", "headers")
-    create_archive(repo_root, install_dir, version, "sources", "sources")
+        if os.path.exists(install_dir / dir / "include"):
+            create_archive(repo_root, install_dir, "include", dir, "headers", group)
+        if os.path.exists(install_dir / dir / "sources"):
+            create_archive(repo_root, install_dir, "sources", dir, "sources", group)
 
 
 def create_archive(
-    repo_root: Path, install_dir: Path, version: str, directory: str, classifier: str
+    repo_root: Path,
+    install_dir: Path,
+    directory: str,
+    artifact_id: str,
+    classifier: str,
+    group: str,
 ):
-    os.chdir(install_dir / directory)
-    directories = glob.glob("*", root_dir=install_dir / directory)
-    for dir in directories:
-        library_dir = install_dir / directory / dir
-        os.chdir(library_dir)
-
-        files = glob.glob("**", recursive=True)
-        artifact_name = f"{dir}-cpp-{version}-{classifier}"
-        with zipfile.ZipFile(install_dir / f"{artifact_name}.zip", "w") as archive:
-            for file in files:
-                archive.write(file)
-            archive.write(repo_root / "ThirdPartyNotices.txt", "ThirdPartyNotices.txt")
-            archive.write(repo_root / "LICENSE.md", "LICENSE.md")
+    os.chdir(install_dir / artifact_id / directory)
+    files = glob.glob("**", recursive=True)
+    artifact_name = f"_GROUP_{group}_ID_{artifact_id}_CLS_{classifier}"
+    with zipfile.ZipFile(install_dir / f"{artifact_name}.zip", "w") as archive:
+        for file in files:
+            archive.write(file)
+        archive.write(repo_root / "ThirdPartyNotices.txt", "ThirdPartyNotices.txt")
+        archive.write(repo_root / "LICENSE.md", "LICENSE.md")
 
 
 if __name__ == "__main__":
