9165d2d6b69ae5cc317b39f5e6256a25596b0801
Gold856 <117957790+Gold856@users.noreply.github.com>
2026-02-07 21:22:45 -0500

    blob

--- shared/bazel/rules/robotpy/hatchlib_native_port/generate_native_lib_files.py
+++ shared/bazel/rules/robotpy/hatchlib_native_port/generate_native_lib_files.py
@@ -54,7 +54,7 @@ class NativelibHook:
     ) -> pathlib.Path:
 
         pcfile_rel = pcfg.get_pc_path()
-        pcfile = self.root_pth / str(pcfile_rel).removeprefix("src/")
+        pcfile = self.root_pth / str(pcfile_rel).replace("src/", "").replace("src\\", "")
         prefix_rel = pcfile_rel.parent
         prefix_path = pcfile.parent
 
@@ -158,7 +158,7 @@ class NativelibHook:
         build_data: dict[str, T.Any],
     ):
         libinit_py_rel = pcfg.get_init_module_path()
-        libinit_py = self.root_pth / str(libinit_py_rel).removeprefix("src/")
+        libinit_py = self.root_pth / str(libinit_py_rel).replace("src/", "").replace("src\\", "")
 
         libdir = prefix_path
         if pcfg.libdir:
